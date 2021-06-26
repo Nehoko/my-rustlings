6951f3665953c88b19380e43666311ae5d9b8ab1
idmikhailov <fun4lsd@gmail.com>
2021-06-26 10:37:14 +0300

    Init commit

---
+++ exercises/variables/variables5.rs
@@ -0,0 +1,9 @@
+// variables5.rs
+// Make me compile! Execute the command `rustlings hint variables5` if you want a hint :)
+
+fn main() {
+    let numberStr = "T-H-R-E-E";
+    println!("Spell a Number : {}", numberStr);
+    let number = 3;
+    println!("Number plus two is : {}", number + 2);
+}
